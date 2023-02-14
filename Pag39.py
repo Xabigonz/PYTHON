70ee093f995abb61e4a38246cad3e2dbd82ab26f
Xabi <xa.gonzalez@aulanz.net>
2023-02-14 17:14:42 +0100

    Terminado con for y while

--- Pag39.py
+++ Pag39.py
@@ -1,4 +1,7 @@
 #Número Aleatorio del 1 al 100.
+
+#Con while:
+
 import random
 
 numeroAdiv = random.randint(0, 100)
@@ -9,6 +12,26 @@ while True:
 
         print(f"El numero es mayor, ")
 
+    elif pregunta > numeroAdiv:
+
+        print(f"El numero es menor, ")
+
+    else:
+
+        print (f"El numero es correcto, es {numeroAdiv}")
+        break
+
+
+#Con for: y el "RANGE" para poder limitar los intentos:
+
+vidas = range(0,3)
+
+for numeroAdiv in vidas:
+    pregunta = int(input("Intenta adivinar el número aleatorio del 1 al 100: "))
+    if pregunta < numeroAdiv:
+        
+        print(f"El numero es mayor, ")
+   
     elif pregunta > numeroAdiv:
 
         print(f"El numero es menor, ")

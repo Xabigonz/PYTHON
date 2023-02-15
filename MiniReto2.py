389808f9b98c4bba05d6ca428315fbc55e79d737
Xabi <xa.gonzalez@aulanz.net>
2023-02-15 17:08:45 +0100

    Añadir lista, asistencia, notas, y nota media.

--- MiniReto2.py
+++ MiniReto2.py
@@ -1,11 +1,30 @@
+import numpy as np
 #Un profesor te pide que hagas una programa que registre la asstencia de alumnos
 
-alumnos = ["Julio", "Arturo", "Dani"]
+#No es la mejor forma de hacerlo con la lista, 
+# sino con el diccionario que es una especie de tabla.
 
+alumnos = ["Julio", "Arturo", "Dani"]
+asistiencia = []
+total = 0
 
 for alumno in alumnos:
     asistido = input(f"{alumno} ha venido? (S/N)")
     if asistido == "S":
         print(f"{alumno} Ha asistido")
+        asistiencia.append({alumno,  "\u2713"})
+        total = total + 1
+        
     else:
         print(f"{alumno} No ha asistido")
+        asistiencia.append({alumno, "x"})
+print(asistiencia)
+print(f"Han asisitido {total} alumnos.")
+
+#Ahora hay que hacer que muestre 
+#la media que han sacado los alumnos en el examén
+
+notas = [4,8,9]
+avg = np.mean(notas)
+print(f"Estás son las notas que habeis obtenido: {notas} \n"
+       f"y estas la media de clase {avg}")

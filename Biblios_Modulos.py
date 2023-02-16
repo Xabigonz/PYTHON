f036995ec21198e3551afd179c4c8d5d892d124a
Xabi <xa.gonzalez@aulanz.net>
2023-02-16 16:42:15 +0100

    cositas

---
+++ Biblios_Modulos.py
@@ -0,0 +1,17 @@
+# from urllib.request import urlopen
+
+# page = urlopen("http://info.cern.ch/")
+# content = page.read() 
+# print(content) 
+# #Esto lo muestra como html.
+
+from urllib3 import requests
+
+currency = "eur"
+basecurrency = "aud"
+
+url = "http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + currency + "&f=" + basecurrency + "&v=1&s=cbr"
+resp = requests.get(url)
+
+print(resp.text)
+print(resp.json())

c684c5f37028a3d566272691589a168cf36346ed
Xabi <xa.gonzalez@aulanz.net>
2023-02-20 15:55:06 +0100

    Bucles while

---
+++ Python7.py
@@ -0,0 +1,22 @@
+i = 10
+
+while i > 0:
+    print("Hola " + str(i))
+    i = i - 1
+    if i == 5:
+        print("i == 5")
+        break
+
+print("final mi loko")
+
+
+#Blucle:
+
+i = 10
+
+while i > 0:
+    print("hola" + str(i))
+    i = i - 1
+
+else:
+    print("El else")

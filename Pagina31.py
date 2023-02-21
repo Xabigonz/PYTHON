e85606723118608b9ecf912caa156e113392c8dc
Xabi <xa.gonzalez@aulanz.net>
2023-02-21 17:24:24 +0100

    Terminado ejercicio en clase. List Comprehension

---
+++ Pagina31.py
@@ -0,0 +1,17 @@
+#Resolver entre toda la clase. Un cadena en formato CSV, intentar dejarla como una simple frase.
+
+s="    122,Python,es,64,un,777,lenguaje,222,de,55,66,programación  "
+
+s = s.strip()
+a = s.split(",")
+b = [str(i) for i in a if not i.isnumeric() ]  #List comprehension.
+print(b)
+texto_final = " "
+
+texto_final = " ".join(str(i)for i in b) # El metodo join permite unir elementos de lista como un string.
+
+print(texto_final)
+
+print(texto_final.upper())
+
+print(texto_final.lower())

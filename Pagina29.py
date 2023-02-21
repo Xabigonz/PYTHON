bfec0d665f48067b229b6d7405fd7d4e651ea1e7
Xabi <xa.gonzalez@aulanz.net>
2023-02-21 16:50:18 +0100

    Coments

--- Pagina29.py
+++ Pagina29.py
@@ -1,22 +1,22 @@
 import getpass
+#Ejercicio 1
 
+contra = str("123456")
+intentos = 5
 
-# contra = str("123456")
-# intentos = 5
-
-# for i in range(intentos):
-#     pregunta = getpass.getpass("Introduzca la contraseña: ")
+for i in range(intentos):
+    pregunta = getpass.getpass("Introduzca la contraseña: ")
     
-#     if pregunta != contra:
-#         intentos -=1
-#         print("La contraseña es incorrecta")
-#         print(f"Le quedan {intentos}.")
+    if pregunta != contra:
+        intentos -=1
+        print("La contraseña es incorrecta")
+        print(f"Le quedan {intentos}.")
 
-#     elif pregunta==contra:
-#         print("Es correcta \n Bienvenido de nuevo REY!" )
-#         break
+    elif pregunta==contra:
+        print("Es correcta \n Bienvenido de nuevo REY!" )
+        break
         
-
+#Mejoras
 
 nContra = getpass.getpass("Inserte su nueva contraseña: ")
 

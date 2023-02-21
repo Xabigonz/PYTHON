582fe6c018c6c2cf1b7210b50f28b05f61eb3557
Xabi <xa.gonzalez@aulanz.net>
2023-02-21 16:49:42 +0100

    Pagina29 pendiente de mejor, pagina 31 iniciar

---
+++ Pagina29.py
@@ -0,0 +1,31 @@
+import getpass
+
+
+# contra = str("123456")
+# intentos = 5
+
+# for i in range(intentos):
+#     pregunta = getpass.getpass("Introduzca la contraseña: ")
+    
+#     if pregunta != contra:
+#         intentos -=1
+#         print("La contraseña es incorrecta")
+#         print(f"Le quedan {intentos}.")
+
+#     elif pregunta==contra:
+#         print("Es correcta \n Bienvenido de nuevo REY!" )
+#         break
+        
+
+
+nContra = getpass.getpass("Inserte su nueva contraseña: ")
+
+i = 0
+
+
+while i < len(nContra):
+    i += 1
+    a=nContra.replace(nContra,"*")
+    
+    
+

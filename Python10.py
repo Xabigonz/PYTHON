2c1c2acd8da404d7ac81d58432f226c140b34909
Xabi <xa.gonzalez@aulanz.net>
2023-02-23 15:33:06 +0100

    Definicion de metodos

---
+++ Python10.py
@@ -0,0 +1,15 @@
+def imprimirMens(lenguaje):
+    print(f"carmelo {lenguaje}")
+    
+
+def calculo(a):
+    return a+a+a+a
+
+#Lo que añades dentro de las parentesis es para darle un valor a la variable.
+
+if __name__ == '__main__':
+    x = imprimirMens("Python")
+    y = calculo(5)
+    print(x, y)
+
+

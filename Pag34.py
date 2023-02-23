bd08d75bb6a9433476efe77e6375afd1802766d7
Xabi <xa.gonzalez@aulanz.net>
2023-02-23 17:52:11 +0100

    Cositas

--- Pag34.py
+++ Pag34.py
@@ -1,22 +1,32 @@
-# import requests
+import requests
 
-# link = "http://info.cern.ch/"
-# r = requests.get(link)
-# print(r.status_code)  # Código de http.
+link = "http://info.cern.ch/"
+r = requests.get(link)
+print(r.status_code)  # Código de http.
 
-# html = r.text
-# lineas_html = html.splitlines()
+html = r.text
+lineas_html = html.splitlines()
 
-# for linea in lineas_html:
-#     linea_encriptada = ""
-#     for caracter in linea:
-#         codigo_ascii = ord(caracter) + 1
-#         linea_encriptada += str(codigo_ascii)
-#     print(linea_encriptada)
+for linea in lineas_html:
+    linea_encriptada = ""
+    for caracter in linea:
+        codigo_ascii = ord(caracter) + 1
+        linea_encriptada += str(codigo_ascii)
+    print(linea_encriptada)
 
 import os
 
-print(os.environ)
-print(os.getlogin)
-print(os.getcwd)
+print(os.environ) # Este diccionario proporciona acceso a las variables de entorno a través de claves.
+print(os.getlogin) #Ver quien y que está usando el usuario actual.
+print(os.getcwd)  
+print(os.listdir())  #Imprime el contenido del directorio actual.
 
+
+
+def eliminadorCarpetas():
+
+    borral = os.rmdir("C:/Users/xabig/OneDrive/Escritorio/BORRA_CARPETA")
+
+    path = "C:/Users/xabig/OneDrive/Escritorio/BORRA_CARPETA"
+    borrado = os.rmdir(path)
+    print(borrado) # Esto imprimirá "None" si se eliminó la carpeta correctamente

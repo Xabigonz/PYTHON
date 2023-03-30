a8e64beac0d7edb3c6917f1c8a2f931c115ddcd9
Xabi <xa.gonzalez@aulanz.net>
2023-03-30 15:11:27 +0200

    Menu_Prjct ended

--- Menu_prjct/app.py
+++ Menu_prjct/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 import os
+
 
 app = Flask(__name__)
 
@@ -23,24 +24,57 @@ class Menu(db.Model):
         self.precio = precio
 
 def admin_Rellenar():
-    plato1 = Menu("Rape", "Rape a la parrilla", 15.00)
-    plato2 = Menu("Rodaballo", "Rodaballo a la parrilla",30.00)
-    plato3 = Menu("Merluza", "Merluza con apionabo y menier", 25.00)
-    plato4 = Menu("Entrecot", "Entroct de Vacuno madurado", 28.00)
-    plato5 = Menu("Solomillo", "Solomillo a la parilla berenjena ahumada y patata soufflé", 45.00)
-    plato6 = Menu("Txuleta", "Txuleta de vacuno Premium, Selección Txogitxu, maduración mínima de 40 días", 78.00) 
-    plato7 = Menu("Carrillera", "Carrillera de vaca estofada con clorifor e Idiazabal",24.00)
-    
-    db.session.add(plato1)
-    db.session.add(plato2)
-    db.session.add(plato3)
-    db.session.add(plato4)
-    db.session.add(plato5)
-    db.session.add(plato6)
-    db.session.add(plato7)
-    
+    #Recoger datos de un formulario
+    nombre = request.form['nombre']
+    descripcion = request.form['descripcion']
+    precio = float(request.form['precio'])
+
+    #Crear nuevo plato:
+    nuevo_menu = Menu(nombreComida=nombre, descripcion=descripcion, precio=precio)
+
+    #Añadir a la base de datos:
+    db.session.add(nuevo_menu)
+
     db.session.commit()
 
+
+    #AÑADIR primeros datos:
+    # plato1 = Menu("Rape", "Rape a la parrilla", 15.00)
+    # plato2 = Menu("Rodaballo", "Rodaballo a la parrilla",30.00)
+    # plato3 = Menu("Merluza", "Merluza con apionabo y menier", 25.00)
+    # plato4 = Menu("Entrecot", "Entroct de Vacuno madurado", 28.00)
+    # plato5 = Menu("Solomillo", "Solomillo a la parilla berenjena ahumada y patata soufflé", 45.00)
+    # plato6 = Menu("Txuleta", "Txuleta de vacuno Premium, Selección Txogitxu, maduración mínima de 40 días", 78.00) 
+    # plato7 = Menu("Carrillera", "Carrillera de vaca estofada con clorifor e Idiazabal",24.00)
+    
+    # db.session.add(plato1)
+    # db.session.add(plato2)
+    # db.session.add(plato3)
+    # db.session.add(plato4)
+    # db.session.add(plato5)
+    # db.session.add(plato6)
+    # db.session.add(plato7)
+    
+    # db.session.commit()
+
+@app.route("/form", methods=['GET','POST'])
+def submit():
+    if request.method == 'POST':
+    
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']
+        precio = request.form['precio']
+        
+        nuevo_menu = Menu(nombreComida=nombre, descripcion=descripcion, precio=precio)
+
+        #Añadir a la base de datos:
+        db.session.add(nuevo_menu)
+
+        db.session.commit()
+
+        return redirect('/')
+    else:
+        return render_template('formulario.html')
 
 
 @app.route("/")
@@ -50,7 +84,7 @@ def menus():
 
 if __name__ == "__main__":
     # with app.app_context():
-    #     # db.drop_all()
-    #     # db.create_all()
+    # #     # db.drop_all()
     #     admin_Rellenar()
+        # db.create_all()
     app.run()

401289f6592efda37fb38347d4c59cc3b315f02f
Xabi <xa.gonzalez@aulanz.net>
2023-04-19 16:14:09 +0200

    Ended

--- EXAMEN_py/Examen2.py
+++ EXAMEN_py/Examen2.py
@@ -1,10 +1,16 @@
+# Pregunta 2:
+# Crear un programa para:
+
+# Imprimir los valores de esta lista, excluyendo los valores en negativo (2)
+# Al imprimir, imprimes "Hola numero 6" si el valor es 6. (1)
 
 
+lista = [5, 3, 12, -6, -3, 1, 6, 8, -12] # CREAR LISTA
 
-lista = [5, 3, 12, -6, -3, 1, 6, 8, -12]
-
-for num in lista:
+for num in lista: 
     if num < 0:
         pass
     else:
         print(f"Numero: {num}")
+
+#EL BUCLE RECORRE LA LISTA Y SI EL num ES < O NO HARÁ NADA Y POR EL CONTRARIO IMPRIMIRA SU VALOR.

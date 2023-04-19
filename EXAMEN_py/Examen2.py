993d4fc2c701edf8ab495ef35c5934dc8d89b6df
Xabi <xa.gonzalez@aulanz.net>
2023-04-19 16:23:16 +0200

    3.er ejercicio y cambio en el primero

--- EXAMEN_py/Examen2.py
+++ EXAMEN_py/Examen2.py
@@ -11,6 +11,37 @@ for num in lista:
     if num < 0:
         pass
     else:
-        print(f"Numero: {num}")
+        print(f"Hola numero: {num}")
 
 #EL BUCLE RECORRE LA LISTA Y SI EL num ES < O NO HARÁ NADA Y POR EL CONTRARIO IMPRIMIRA SU VALOR.
+
+
+
+# Pregunta 3:
+# Teniendo un rectángulo de 10 metros de base y 3 metros de altura, crear una función para calcular su área.
+
+# Acciones:
+
+# Crear la función (2)
+# Ejecutar la función con valores que proporciona el usuario (1)
+# Modificar la función para que si el usuario ejecuta area(10), por defecto coge el valor 3 de altura (1)
+
+
+
+# INFORMACION DEL TRIANGULO
+baseRec = 10
+altRec = 3
+
+
+
+pregUser = int(input("Desea insertar valores o calcular el area del triangulo? (1/2)"))
+while True:
+    if pregUser == 1:
+        baseRec = int(input("Inserta el balor de la base: "))
+        altRec = int(input("Inserta el balor de la altura: "))
+        area = (baseRec * altRec)/2
+    elif pregUser == 2:
+        area = (baseRec*altRec)/2        
+        print(f"El area del triangulo es {area}")
+
+        
